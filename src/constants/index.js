1b9bbab7c2ece6c6da4fef69af84f3b9d74ad8dc
syed noman <syed@192.168.0.102>
2023-07-06 12:59:28 +0500

    fixed a typo

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,4 +1,4 @@
-export const apiKey = 'place your openAI api kye here';
+export const apiKey = 'place your openAI api key here';
 // in some cases your api key maybe already expired
 // try to use a new account to create an api key
 
