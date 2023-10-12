bc1a55e5461cd47a3050e2c05f73b16398835424
KyNgo <106151210+KyNgoAmela@users.noreply.github.com>
2023-10-12 07:16:13 +0700

    push

--- App.js
+++ App.js
@@ -1,7 +1,8 @@
-import React from 'react';
+import React ,{useState, useEffect} from 'react';
 import {StyleSheet, View} from 'react-native';
 import NavigatorOne from './src/navigation';
 import TabAuthen from './src/navigation/TabAuthen';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const Screen1 = () => {
   return <View style={styles.screen1} />;
@@ -12,6 +13,15 @@ const Screen2 = () => {
 };
 const a = 1;
 export default function App() {
+  const [token, setToken]= useState()
+  useEffect(() => {
+    renderToken()
+  });
+  const renderToken =async()=>{
+   const resToken = await AsyncStorage.getItem('chatFaceId');
+   setToken(resToken)
+  }
+  console.log('token',token)
   if (a === 1) {
     return <TabAuthen />;
   }
